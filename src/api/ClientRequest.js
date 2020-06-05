863be1c5de4c9beeca7a9078c55ada192048c190
Mohammad Hossein Azimi <mohammadazimi91@gmail.com>
2020-06-06 01:02:44 +0430

    fixed an issue in ClinetRequest.js

--- src/api/ClientRequest.js
+++ src/api/ClientRequest.js
@@ -6,34 +6,34 @@ const BASE_URL = "domain.com";
 const logStyle = 'color:#F00;font-size:16px';
 
 //TODO: set header and authorization
-const requestHttp = ({method = 'get', url, data, ...options}) => {
-    axios({
-        method,
-        url,
-        data,
-        ...options,
-      })
-      .then(response => {
-        console.log(`%chttp request ${method} response`, logStyle, response.data);
-        return {result: response.data, ok: true};
-      })
-      .catch(e=>{        
-        return errorHandler(e);
-      });
+const requestHttp = ({ method = 'get', url, data, ...options }) => {
+  return axios({
+    method,
+    url,
+    data,
+    ...options,
+  })
+    .then(response => {
+      console.log(`%chttp request ${method} response`, logStyle, response.data);
+      return { result: response.data, ok: true };
+    })
+    .catch(e => {
+      return errorHandler(e);
+    });
 }
 
 const getRequest = options => {
-    return requestHttp({method: 'get', ...options});
+  return requestHttp({ method: 'get', ...options });
 }
 
 const postRequest = options => {
-    return requestHttp({method: 'post', ...options});
+  return requestHttp({ method: 'post', ...options });
 }
 
 const errorHandler = error => {
-    // TODO: 401 and unAuthorized request should be handled
-    console.log(`%chttp request error`, logStyle, error);
-    return  {result: error, notOk: true}
+  // TODO: 401 and unAuthorized request should be handled
+  console.log(`%chttp request error`, logStyle, error);
+  return { result: error, notOk: true }
 }
 
-export {getRequest, postRequest};
+export { getRequest, postRequest };
